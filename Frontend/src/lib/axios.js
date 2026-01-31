11b40f1312a5975bea440498c7cbce165d0593de
Ratnabh <sratnabh@gmail.com>
2026-01-31 12:52:32 +0530

    axios config changes

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api",
+  baseURL: "https://gossipy-1.onrender.com",
   withCredentials: true,
 });
+
+// import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api",
