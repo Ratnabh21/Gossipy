036084fc99489718d91c6d7de1fc9da7e5edcb97
Ratnabh <sratnabh@gmail.com>
2026-01-31 13:39:53 +0530

    authstore config change

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { connect, io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000" : "/";
+const BASE_URL = "https://gossipy-1.onrender.com";
 
 export const useAuthStore = create ((set, get) => ({
     authUser: null,
