990de71d0a6b60195e5c8fd8df92d719cd4c2130
Ratnabh <sratnabh@gmail.com>
2026-01-31 13:03:48 +0530

    axios config change

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://gossipy-1.onrender.com",
+  baseURL: "https://gossipy-1.onrender.com/api",
   withCredentials: true,
 });
 
